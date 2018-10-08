0de895886db0ab7e1bddbfb65ebf92cfa1bfdaab
Joshua Butt <josh.ja.butt@gmail.com>
2018-10-09 00:29:59 +1000

    Impliment reddit oauth based authentication process
    develop `/vote/auth`
    require auth to access survey questions

--- routes.js
+++ routes.js
@@ -1,14 +1,82 @@
 const fs = require('fs');
+const crypto = require('crypto');
+var request = require('request');
 const debug = require('debug')('routes');
 
+const secrets = JSON.parse(fs.readFileSync(`${__dirname}/secrets.json`, 'utf-8'));
+
 module.exports = (express) => {
+
+    // GET /vote/auth
+    express.get('/vote/auth', (req, res) => {
+
+        // Verify authentication state
+        if (req.session.state !== req.query.state) {
+            var error = {};
+            error.survey_name = "Error 500: Internal Server Error";
+            error.survey_description = "An error has occured, please try again.";
+            error.path = req.path;
+            return res.status(500).render('pages/error', error);
+        }
+
+        // Get information from reddit
+        request.post('https://www.reddit.com/api/v1/access_token', {
+            auth: {
+                username: secrets.client_id,
+                password: secrets.client_secret
+            },
+            body: `grant_type=authorization_code&code=${req.query.code}&redirect_uri=${secrets.redirect_uri}`
+        }, function (error, response, body) {
+
+            var token = JSON.parse(body).access_token;
+
+            // Get client username
+            request.get('https://oauth.reddit.com/api/v1/me', {
+                auth: {
+                    bearer: token
+                },
+                headers: {
+                    'User-Agent': secrets.user_agent
+                }
+            }, function (error, response, body) {
+                req.session.reddit_username = JSON.parse(body).name;
+
+                // Check if client mods subreddit
+                request.get({
+                    url: 'https://oauth.reddit.com/subreddits/mine/moderator',
+                    auth: {
+                        bearer: token
+                    },
+                    headers: {
+                        'User-Agent': secrets.user_agent
+                    }
+                }, function (error, response, body) {
+                    req.session.is_mod = false;
+
+                    for (var key in JSON.parse(body).data.children) {
+                        subreddit = JSON.parse(body).data.children[key];
+                        if (subreddit.data.url === '/r/pokemon/')
+                            req.session.is_mod = true;
+                    }
+
+                    req.session.auth = true;
+
+                    // Redirect to last survey
+                    if (req.session.survey_name)
+                        return res.redirect(`/vote/${req.session.survey_name}`);
+                    else
+                        return res.redirect('/');
+                });
+            });
+        });
+    });
 
     // GET /vote/somevotename
     express.get('/vote/:vote_name', (req, res) => {
 
         // Handle if survey doesn't exist
         if (!fs.existsSync(`${__dirname}/surveys/${req.params.vote_name}.json`)) {
-            var error = { };
+            var error = {};
             error.survey_name = "Error 404: Survey not found";
             error.survey_description = "The survey you have specified could not be found.";
             error.path = req.path;
@@ -19,23 +87,50 @@ module.exports = (express) => {
         var survey = JSON.parse(fs.readFileSync(`${__dirname}/surveys/${req.params.vote_name}.json`, 'utf-8'));
         survey.path = req.path;
 
+        // Handle if the user is not logged in
+        if (!req.session.auth) {
+            req.session.survey_name = req.params.vote_name;
+            req.session.state = crypto.randomBytes(20).toString('hex');
 
-        // TODO: If authenticated, get the questions
-        /*if (req.session.auth)
-        {
-            survey.questions = [ ];
-        }*/
+            survey.questions = null;
+            survey.require_auth = true;
+            survey.client_id = secrets.client_id;
+            survey.auth_state = req.session.state;
+            survey.redirect_uri = secrets.redirect_uri;
+
+            return res.status(200).render('pages/vote', survey);
+        }
+
+        // Handle if user is not authorised
+        if (!req.session.is_mod && survey.requires_mod) {
+            var error = {};
+            error.survey_name = "Error 401: Unauthorized";
+            error.survey_description = "You are not authorized to access this survey.";
+            error.path = req.path;
+            return res.status(401).render('pages/error', error);
+        }
+
+        // TODO: Handle if the user has already voted
+        if (!true) {
+            var error = {};
+            error.survey_name = "Error 401: Unauthorized";
+            error.survey_description = "You are not authorized to access this survey.";
+            error.path = req.path;
+            return res.status(401).render('pages/error', error);
+        }
+
+        survey.reddit_username = req.session.reddit_username
 
         res.status(200).render('pages/vote', survey);
     });
-    
+
     // POST /vote/somevotename/response
     express.post('/vote/:vote_name/response', (req, res) => {
 
         // ignore any response where the user hasn't otherwise authenticated
         if (!req.session.auth)
-            return res.status(500);
-        
+            return res.status(401);
+
         // req.body will be an array
         var response = req.body;
 
@@ -49,7 +144,7 @@ module.exports = (express) => {
     // GET /vote/somevotename/results
     express.get('/vote/:vote_name/results', (req, res) => {
 
-        var survey = { };
+        var survey = {};
 
         // TODO: determine if authentication is required (e.g. for mod vote)
 
@@ -62,12 +157,5 @@ module.exports = (express) => {
         res.status(200).render('pages/results', survey);
     });
 
-    // POST /vote/auth/someredditusername
-    express.post('/vote/auth/:username', (req, res) => {
 
-        // TODO: authenticate users
-        req.session.auth = true;
-        
-        res.status(req.session.auth ? 200 : 500);
-    })
 };
