6de83dd6d25dbf77165d887d08b3a27f5aa94fa7
Leif Walker-Grant <hay@leif.software>
2018-10-09 19:00:41 +0100

    Disable next/submit buttons until response provided
    Additionally, retrieve responses from bool-type questions

--- static/vote/js/votr.js
+++ static/vote/js/votr.js
@@ -4,30 +4,95 @@ const setResponse = (id, value) => {
     submission_data.responses[`q${id}`] = value;
 };
 
+const responseSelectorTable = {
+    bool: {
+        radio: 'input[type="radio"]:checked',
+        dropdown: 'select',
+        default: 'input[type="radio"]:checked',
+    },
+    multi: {
+    },
+    int: {
+    },
+    text: {
+    }
+};
+
 $(document).ready(function() {
     var activeQuestionPanel = $('section:first-of-type');
 
+    // at presently only checks that a value is set
+    const canMoveNextQuestion = (obj) => {
+        var key = obj.attr('data-key');
+        var form = `#question_form_${key}`;
+
+        var resp_type = $(form).attr('data-response-type');
+        var display_as = $(form).attr('data-display-as');
+        var selector = responseSelectorTable[resp_type][display_as];
+
+        var response = $(`${form} ${selector}`).val();
+        var has_response = response  !== undefined;
+
+        var button_id = `#question_next_${key}`;
+        if (has_response) $(button_id).removeAttr('disabled');
+        else $(button_id).attr('disabled');
+    }
+
+    $('.question_form').ready(function(e) {
+        canMoveNextQuestion($(this));
+    });
+
+    $('.question_form').click(function(e) {
+        canMoveNextQuestion($(this));
+    });
+
+    $('.question_form').keyup(function(e) {
+        canMoveNextQuestion($(this));
+    });
+
     $('.next-section').click(function(e) {
         activeQuestionPanel = $(this).parents('section').next();
-        console.log("next section is " + activeQuestionPanel.attr("id"));
         $('body').scrollTo(activeQuestionPanel, 1000, { easing: 'easeInOutQuint' });
     });
 
     $('.prev-section').click(function(e) {
         activeQuestionPanel = $(this).parents('section').prev();        
-        console.log("next section is " + activeQuestionPanel.attr("id"));
         $('body').scrollTo(activeQuestionPanel, 1000, { easing: 'easeInOutQuint' });
     });
 
     $('.record-response').click(function(e) {
-        // TODO: record-responses
+        var key = $(this).attr('data-key');
+        var form = `#question_form_${key}`;
+
+        var resp_type = $(form).attr('data-response-type');
+        var display_as = $(form).attr('data-display-as');
+        var selector = responseSelectorTable[resp_type][display_as];
+
+        if (!selector)
+        {
+            console.log(`Warning! No response selector defined for '${resp_type}' as '${display_as}'`);
+            setResponse(key, 'ERR_NO_SELECTOR');
+            return;
+        }
+
+        var response = $(`${form} ${selector}`).val();
+
+        // response should only ever be undefined in the event that user clicks next having not provided a response
+        // note that, when skipping, we insert null instead
+        if (response === undefined)
+            response = 'ERR_REQUIRED_RESPONSE_NOT_GIVEN';
+
+        setResponse(key, response);
     });
 
     $('.skip-response').click(function(e) {
-        // TODO: record skipped response
+        var key = $(this).attr('data-key');
+        setResponse(key, null);
     });
 
     $('.send-survey-response').click(function(e) {
+
+        console.log(JSON.stringify(submission_data.responses));
 
         $.ajax({
             url: '/vote/ui_test/response',
